b41a213dfc9019d7213630fee03e2618ec0acb32
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-03-26 16:01:52 -0400

    added comments to server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
 const AuthRouter = require('./routes/AuthRouter')
+const CommentRouter = require('./routes/CommentRouter')
 
 const app = express()
 
@@ -13,5 +14,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/auth', AuthRouter)
+app.use('/comments', CommentRouter)
 
 app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`))
