d30d3550dfe51317117b074aa3bad91d7a0dee85
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-03-30 11:41:02 -0400

    buttons on home page are functional

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,19 @@
 
 import '../App.css'
+import { Link } from 'react-router-dom'
 const Home = ()=> {
   return(
     <section className='startSection'>
       
       <h1 className="WelcomeMessage">Welcome to Just rides</h1>
+      <Link to={`/api/auth/register`}>
+        
       <button className="SignUp">Sign Up</button>
+      </Link>
+      <Link to={`/api/auth/login`}>
+
       <button className="LogIn">Log In</button>
+      </Link>
     </section>
   )
 }
