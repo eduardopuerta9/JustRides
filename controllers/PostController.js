3a75f1b63119ee863159fefedd2bbb7c342ee6a1
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-03-30 10:24:04 -0400

    front end installs complete

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -10,6 +10,7 @@ const GetPosts = async (req, res) => {
 }
 
 const CreatePost = async (req, res) => {
+  console.log(res.locals)
   try {
     const post = await Post.create({ ...req.body })
     res.send(post)
