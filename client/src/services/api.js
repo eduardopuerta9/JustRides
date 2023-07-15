ebb20b72791cf66a0b04403b0ca72e01aa5d7d11
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-07-15 14:08:39 -0400

    changes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'https://just-rides-app-api.onrender.com'
+const BASE_URL = 'https://just-rides-app-api.onrender.com'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
